9d75ad97bdc812fe23110e45b14c94196ff61868
FerdinandHu <hujue8604@gmail.com>
2026-09-11 19:13:27 +0800

    Add aesdsocket to aesd-assignments package and configure QEMU port forwarding

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,11 +1,10 @@
-
 ##############################################################
 #
 # AESD-ASSIGNMENTS
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 9a5280baaf498cc44a650217692aab9bf70747cb
+AESD_ASSIGNMENTS_VERSION = b01531878dd9e999213fef977926da94d77d2850
 # Use SSH so the automated test environment can authenticate with its deploy key.
 AESD_ASSIGNMENTS_SITE = git@github.com:Ferdinandhu000/aeld-assignment-3-and-later.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
@@ -13,6 +12,7 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
@@ -21,6 +21,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/writer
 	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
 	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
